5e51c2b3b5fcf9d389673810d443fbf9f9cb997b
artyom-beilis <artyom-beilis@831565cc-8e21-4ad5-8334-98019c97bd36>
2007-11-17 08:59:56 +0000

    Created branch for development of dynamic templates
    git-svn-id: svn://svn.code.sf.net/p/cppcms/code/framework@12 831565cc-8e21-4ad5-8334-98019c97bd36

---
+++ branches/dynamic_templates_support/worker_thread.h
@@ -0,0 +1,50 @@
+#ifndef _WORKER_THREAD_H_
+#define _WORKER_THREAD_H_
+
+#include <pthread.h>
+#include <sstream>
+#include <string>
+
+#include "textstream.h"
+
+#include "cgicc/Cgicc.h"
+#include "cgicc/HTTPHTMLHeader.h"
+#include "cgicc/HTTPStatusHeader.h"
+#include "cgicc/HTMLClasses.h"
+#include <memory>
+
+#include "FCgiIO.h"
+#include "http_error.h"
+
+using namespace std;
+using cgicc::CgiEnvironment;
+using cgicc::FCgiIO;
+using cgicc::Cgicc;
+using cgicc::HTTPHeader;
+
+
+
+
+class Worker_Thread {
+	auto_ptr<FCgiIO> io;
+protected:	
+	auto_ptr<Cgicc> cgi;
+	CgiEnvironment const *env;
+
+	Text_Stream out;
+	auto_ptr<HTTPHeader> response_header;
+	void set_header(HTTPHeader*h){response_header=auto_ptr<HTTPHeader>(h);};
+	virtual void main();
+public:
+	int id;
+	pthread_t pid;
+
+
+	void run(FCGX_Request *req);
+
+	Worker_Thread();
+	virtual ~Worker_Thread(){};
+	virtual void init() {};
+};
+
+#endif
