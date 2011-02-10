cbbd12c012f05f0d4b30660fdb0a8d7b3f386e34
artyom-beilis <artyom-beilis@831565cc-8e21-4ad5-8334-98019c97bd36>
2011-02-10 08:21:50 +0000

    Moved uncorrect URL
    git-svn-id: svn://svn.code.sf.net/p/cppcms/code/framework@1684 831565cc-8e21-4ad5-8334-98019c97bd36

--- blog/trunk/sql/upgrade-1-to-2.sql
+++
@@ -1,7 +0,0 @@
-begin;
-insert into text_options values('blog_title',(select value from options where id=0)); 
-insert into text_options values('blog_description',(select value from options where id=1)); 
-insert into text_options values('contact',(select value from options where id=2)); 
-update text_options set value='2' where id='dbversion';
-drop table options;
-commit;
